d2c9cea9bebc6ec1dbc43207440973f0c73596de
zahraufairah11 <ufairahzahra2@gmail.com>
2025-03-19 09:54:14 +0700

    buatdashboard

--- billiard/routes/web.php
+++ billiard/routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/items', [ItemController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
