4ec800312a6f03a2ba89b23e3e0f334fae2cdbeb
grace031105 <gracesimanungkalit19@gmail.com>
2025-04-28 19:52:17 +0700

    grace mengedit web.php

--- billiard/resources/views/dashboard.blade.php
+++ billiard/resources/views/dashboard.blade.php
@@ -1,10 +1,3 @@
-<?php 
-session_start();
-if (!isset($_SESSION['nama_pengguna'])) {
-    header("Location: login.php");
-    exit();
-}
-?>
 <!DOCTYPE html>
 <html lang="id">
 <head>
--- billiard/routes/web.php
+++ billiard/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'authenticate']);
