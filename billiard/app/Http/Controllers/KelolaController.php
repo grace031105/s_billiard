352e4a2e6007e0b75905a90632f28d16e7daf27b
zahraufairah11 <ufairahzahra2@gmail.com>
2025-06-15 16:36:05 +0700

    mengubah kode kelola controller

--- billiard/app/Http/Controllers/KelolaController.php
+++ billiard/app/Http/Controllers/KelolaController.php
@@ -8,43 +8,46 @@ use Illuminate\Support\Facades\Auth;
 
 class KelolaController extends Controller
 {
-    // Tampilkan seluruh data meja
+    protected $meja;
+
+    public function __construct(Meja $meja)
+    {
+        $this->meja = $meja;
+    }
+
     public function show()
     {
-        $mejas = Meja::all();
+        $mejas = $this->meja->all();
         return view('pages.kelola_meja', compact('mejas'));
     }
 
-    // Simpan data meja baru
     public function simpan(Request $request)
     {
         $fileName = null;
-
         if ($request->hasFile('foto_meja')) {
             $file = $request->file('foto_meja');
             $fileName = time() . '_' . $file->getClientOriginalName();
             $file->move(public_path('images'), $fileName);
         }
 
-        $meja = new Meja;
-        $meja->kode_meja     = $request->input('kode_meja');
-        $meja->nama_meja     = $request->input('nama_meja');
-        $meja->tipe_meja     = $request->input('tipe_meja');
+        $meja = $this->meja->newInstance();
+        $meja->kode_meja = $request->input('kode_meja');
+        $meja->nama_meja = $request->input('nama_meja');
+        $meja->tipe_meja = $request->input('tipe_meja');
         $meja->harga_per_jam = $request->input('harga_per_jam');
-        $meja->foto_meja     = $fileName;
-        $meja->status_meja   = $request->input('status_meja');
-        $meja->id_pemilik    = Auth::guard('pemilik')->user()->id_pemilik;
+        $meja->foto_meja = $fileName;
+        $meja->status_meja = $request->input('status_meja');
+
+        $meja->id_pemilik = Auth::guard('pemilik')->user()->id_pemilik;
 
         $meja->save();
 
         return redirect()->back()->with('success', 'Data berhasil disimpan!');
     }
 
-    // Hapus data meja berdasarkan ID
     public function delete($id)
     {
-        $meja = Meja::where('id_meja', $id)->first();
-
+        $meja = $this->meja->find($id);
         if ($meja) {
             $meja->delete();
             return redirect()->back()->with('success', 'Meja berhasil dihapus');
@@ -53,39 +56,36 @@ class KelolaController extends Controller
         }
     }
 
-    // Tampilkan form edit dengan data meja tertentu
     public function edit($id_meja)
     {
-        $mejas = Meja::all();
-        $meja  = Meja::findOrFail($id_meja);
-
+        $mejas = $this->meja->all();
+        $meja = $this->meja->findOrFail($id_meja);
         return view('pages.kelola_meja', compact('mejas', 'meja'));
     }
 
-    // Proses update data meja
     public function update(Request $request, $id_meja)
     {
         $request->validate([
-            'nama_meja'     => 'required',
-            'tipe_meja'     => 'required',
+            'kode_meja' => 'required',
+            'nama_meja' => 'required',
+            'tipe_meja' => 'required',
             'harga_per_jam' => 'required|numeric',
         ]);
 
-        $meja = Meja::findOrFail($id_meja);
+        $meja = $this->meja->findOrFail($id_meja);
 
-        // Cek apakah ada upload foto baru
+        $meja->kode_meja = $request->kode_meja;
+        $meja->nama_meja = $request->nama_meja;
+        $meja->tipe_meja = $request->tipe_meja;
+        $meja->harga_per_jam = $request->harga_per_jam;
+        $meja->status_meja = $request->status_meja;
+
         if ($request->hasFile('foto_meja')) {
             $file = $request->file('foto_meja');
             $fileName = time() . '_' . $file->getClientOriginalName();
             $file->move(public_path('images'), $fileName);
             $meja->foto_meja = $fileName;
         }
-
-        // Update data lainnya
-        $meja->nama_meja     = $request->input('nama_meja');
-        $meja->tipe_meja     = $request->input('tipe_meja');
-        $meja->harga_per_jam = $request->input('harga_per_jam');
-        $meja->status_meja   = $request->input('status_meja');
 
         $meja->save();
 
