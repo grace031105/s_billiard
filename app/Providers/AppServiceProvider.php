8540c7f987bd67ba700d1c27199826e3c8292f37
grace031105 <gracesimanungkalit19@gmail.com>
2025-06-22 11:00:00 +0700

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\View;
-use App\Models\Reservasi;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,12 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('*', function ($view) {
-            $notifikasiReservasi = Reservasi::where('status', 'menunggu_konfirmasi')
-                                    ->orderBy('id_reservasi', 'desc')
-                                    ->limit(7)
-                                    ->get();
-            $view->with('notifikasiReservasi', $notifikasiReservasi);
-        });
+        //
     }
 }
